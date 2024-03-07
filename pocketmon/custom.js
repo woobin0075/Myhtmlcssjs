791c2fc03211f0f497d8d8dc25b31fb0fce724cc
chowoobin <49549052+woobin0075@users.noreply.github.com>
2024-03-07 14:02:00 +0900

    Add files via upload

---
+++ pocketmon/custom.js
@@ -0,0 +1,17 @@
+$('.pocketbox:nth-child(1)').hover(function(){
+    $('.pocketbox:nth-child(1) .hoverbox').css({'opacity':'0.7'})
+}, function(){
+    $('.pocketbox:nth-child(1) .hoverbox').css({'opacity':'0'})
+})
+
+$('.pocketbox:nth-child(2)').hover(function(){
+    $('.pocketbox:nth-child(2) .hoverbox').css({'opacity':'0.7'})
+}, function(){
+    $('.pocketbox:nth-child(2) .hoverbox').css({'opacity':'0'})
+})
+
+$('.pocketbox:nth-child(3)').hover(function(){
+    $('.pocketbox:nth-child(3) .hoverbox').css({'opacity':'0.7'})
+}, function(){
+    $('.pocketbox:nth-child(3) .hoverbox').css({'opacity':'0'})
+})
