f585904841ac671a6323366bbffef397af8f02fd
chowoobin <49549052+woobin0075@users.noreply.github.com>
2024-03-07 14:01:37 +0900

    Add files via upload

---
+++ hoverballoon/custom.js
@@ -0,0 +1,27 @@
+$('.box img:nth-of-type(1)').hover(function(){
+    $('.box .ballon:nth-of-type(1)').fadeIn();
+    
+}, function(){
+    $('.box .ballon:nth-of-type(1)').fadeOut();
+})
+
+$('.box img:nth-of-type(2)').hover(function(){
+    $('.box .ballon:nth-of-type(2)').fadeIn();
+    
+}, function(){
+    $('.box .ballon:nth-of-type(2)').fadeOut();
+})
+
+$('.box img:nth-of-type(3)').hover(function(){
+    $('.box .ballon:nth-of-type(3)').fadeIn();
+    
+}, function(){
+    $('.box .ballon:nth-of-type(3)').fadeOut();
+})
+
+$('.box img:nth-of-type(4)').hover(function(){
+    $('.box .ballon:nth-of-type(4)').fadeIn();
+    
+}, function(){
+    $('.box .ballon:nth-of-type(4)').fadeOut();
+})
