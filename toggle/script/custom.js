9f19f807b445c268c4229560db53090ee48b5290
chowoobin <49549052+woobin0075@users.noreply.github.com>
2024-03-07 14:04:29 +0900

    Add files via upload

---
+++ toggle/script/custom.js
@@ -0,0 +1,47 @@
+/*$('.show-btn').click(function(){
+    $('.container div').show();
+})
+
+$('.hide-btn').click(function(){
+    $('.container div').hide();
+})
+
+$('.toggle-btn').click(function(){
+    $('.container div').toggle();
+})*/
+
+/*$('.show-btn').click(function(){
+    $('.container div').slideDown();
+})
+
+$('.hide-btn').click(function(){
+    $('.container div').slideUp();
+})
+
+$('.toggle-btn').click(function(){
+    $('.container div').slideToggle();
+})*/
+
+/*$('.show-btn').click(function(){
+    $('.container div').fadeIn(1000);
+})
+
+$('.hide-btn').click(function(){
+    $('.container div').fadeOut(2000);
+})
+
+$('.toggle-btn').click(function(){
+    $('.container div').fadeToggle(3000);
+})*/
+
+$('.add-btn').click(function(){
+    $('.container > div').addClass('active');
+})
+
+$('.remove-btn').click(function(){
+    $('.container > div').removeClass('active');
+})
+
+$('.toggle-btn').click(function(){
+    $('.container > div').toggleClass('active');
+})
